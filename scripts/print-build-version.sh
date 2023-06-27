1c6043e67ef2c7ceac450864b6edb791bb39579e
Martin Carpella <martin.carpella@gmx.net>
2023-06-27 20:51:21 +0200

    Fix override detection in print-build-version.sh

--- scripts/print-build-version.sh
+++ scripts/print-build-version.sh
@@ -7,5 +7,5 @@ GIT_REVISION="$(git rev-parse HEAD)"
 GIT_DIRTY=""
 [ -n "$(git status -s)" ] && GIT_DIRTY="-dirty"
 OVERRIDE=""
-[ -e ".buildoverride" ] && find .buildoverride 2>/dev/null | head -n1 | grep . >/dev/null && OVERRIDE="-with-overrides" || true
+[ -e ".buildoverride" ] && find .buildoverride -type f 2>/dev/null | head -n1 | grep . >/dev/null && OVERRIDE="-with-overrides" || true
 echo "${GIT_REVISION}${GIT_DIRTY}${OVERRIDE}"
